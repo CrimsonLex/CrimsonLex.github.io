3149ea306179d57387e107ef24e3c4ca9497e58c
Juan Camilo <61602045+juanmiloz@users.noreply.github.com>
2023-05-25 09:38:03 -0500

    fix error

--- src/js/statics.js
+++ src/js/statics.js
@@ -1,5 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, updateDoc, increment, setDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  doc,
+  updateDoc,
+  increment,
+  setDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC9fE3Izyd0YsmMG9ArFUuqAtIZsYQzPJQ",
@@ -7,22 +13,28 @@ const firebaseConfig = {
   projectId: "planify-93ca2",
   storageBucket: "planify-93ca2.appspot.com",
   messagingSenderId: "353057198767",
-  appId: "1:353057198767:web:3f28e6c95a604537e33a5a"
+  appId: "1:353057198767:web:3f28e6c95a604537e33a5a",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const btn = document.getElementById("counter");
-const counterRef = doc(db, "statics", "counterDoc")
+const a = async (params) => {
+  const counterRef = doc(db, "statics", "counterDoc");
 
-await setDoc(doc(db, "statics", "Nothing"), {
-  name: "to stablish conection"
-});
+  await setDoc(doc(db, "statics", "Nothing"), {
+    name: "to stablish conection",
+  });
 
+  
 btn.addEventListener("click", async () => {
-    console.log('entra')
-    await updateDoc(counterRef, {
-      counter: increment(1)
-    })
+  console.log("entra");
+  await updateDoc(counterRef, {
+    counter: increment(1),
+  });
 });
+
+};
+
+a()
